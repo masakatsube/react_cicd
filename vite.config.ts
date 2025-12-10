4fb7d8bc921d2ccf8ebe9b005f8fd6f8a7f23538
masakatsube <mkatsube0605@yahoo.co.jp>
2025-12-10 16:59:13 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true, // 環境に合わせて
     environment: 'jsdom', // Reactプロジェクトの場合
-    
+    setupFiles: ['./tests/setup.ts'],
     coverage: {
       provider: 'c8', // デフォルトはc8。必要に応じて設定
       reporter: ['text', 'json', 'cobertura'], // CLIにも表示しつつ、octocov用にcoberturaを出力
